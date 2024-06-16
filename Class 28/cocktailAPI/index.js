4e5255e950825e8afe840a07f579671393af6b68
Jorge Machuca <jmachuca06@gmail.com>
2024-06-16 07:58:29 -0400

    finished cocktail api challenge

---
+++ Class 28/cocktailAPI/index.js
@@ -0,0 +1,57 @@
+class CocktailService {
+  static ENDPOINT = "https://www.thecocktaildb.com/api/json/v1/1/search.php?";
+  constructor() {}
+
+  static getCocktails(query) {
+    const getData = async (query) => {
+      const params = new URLSearchParams({ s: query }).toString();
+
+      // Make fetch request
+      const response = await fetch(`${this.ENDPOINT}${params}`);
+      // parse response to json
+      const result = await response.json();
+
+      return result["drinks"] ?? null;
+    };
+    return getData(query);
+  }
+}
+
+const queryInput = document.querySelector("#query");
+const form = document.querySelector("form");
+const cocktailContainer = document.querySelector("#cocktails__container");
+
+function onSubmitHandler(e) {
+  e.preventDefault();
+  cocktailContainer.innerHTML = "";
+  // const formData = new FormData(e.currentTarget);
+  // console.log(formData);
+  const query = queryInput.value;
+  CocktailService.getCocktails(query).then((data) => {
+    console.log(data);
+    data.forEach((element) => {
+      const cocktail = new Cocktail(element);
+      cocktail.render(cocktailContainer);
+    });
+  });
+  queryInput.value = "";
+}
+
+class Cocktail {
+  constructor(obj) {
+    this.drinkId = obj["idDrink"];
+    this.strAlcoholic = obj["strAlcoholic"];
+    this.name = obj["strDrink"];
+    this.thumbnail = obj["strDrinkThumb"];
+    this.instructions = obj["strInstructions"];
+  }
+
+  render(parent) {
+    const elem = document.createElement("div");
+
+    elem.innerHTML = `<img src="${this.thumbnail}" width="100px" height="100px"/> <h3>${this.name}</h3><p>Type: ${this.strAlcoholic}</p> <p>${this.instructions}</p>`;
+    parent.appendChild(elem);
+  }
+}
+
+form.addEventListener("submit", onSubmitHandler);
